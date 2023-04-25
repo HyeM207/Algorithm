a683beb45cbb905fe377874d94c53e672b8e134f
HyeM207 <hmk9667@gmail.com>
2023-04-25 11:07:21 +0900

    [Prg] 로또의 최고 순위와 최저 순위 복습
    2회독

--- Implementation/[Prg] 로또의 최고 순위와 최저 순위.py
+++ Implementation/[Prg] 로또의 최고 순위와 최저 순위.py
@@ -23,3 +23,26 @@ def solution(lottos, win_nums):
     answer.append(rank[right_num+zero_num])
     answer.append(rank[right_num])
     return answer
+
+
+'''
+# #2-230425 성공
+    정확성: 100.0
+    합계: 100.0 / 100.0
+    풀이 : 안 보고 풀었는데 첫번째 풀이할때와 거의 똑같은 방법으로 풀이하였다; 
+'''
+def solution(lottos, win_nums):
+    answer = []
+    rank = { 6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6 } # 일치개수 : 순위
+    num_same = 0
+    num_zero = 0
+    
+    for l in lottos : 
+        if l in win_nums : 
+            num_same += 1 
+        if l == 0 :
+            num_zero += 1
+
+    answer.append(rank[num_same+num_zero])
+    answer.append(rank[num_same])
+    return answer
