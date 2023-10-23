b6b452a8fdbf33ac34df361e59751ba9ebeac04f
HyeM207 <hmk9667@gmail.com>
2023-10-23 14:00:06 +0900

    [Chore] 기존에 푼 leetcode 문제 삭제
    - Leethub 플러그인 형식에 맞추기 위해 삭제함

--- LeetCode/Easy/[LeetCode] 1. Two Sum.py
+++
@@ -1,8 +0,0 @@
-# Solution 1
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range (0,len(nums)-1):
-            for j in range (i+1,len(nums)) :
-                if nums[i] +nums[j] == target:
-                    return [i,j]
-                    break
