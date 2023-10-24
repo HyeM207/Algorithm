93e329e47e0d4f3257ad4cb760f0641053e00328
Hyemin Kim <hmk9667@gmail.com>
2023-10-24 14:59:18 +0900

    Time: 81 ms (78.31%), Space: 17.5 MB (97.30%) - LeetHub

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,10 +1,4 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # pop 을 이용한 풀이 => O(n)
-        i = 0
-        while i < len(nums): 
-            if i>0 and nums[i-1] == nums[i]:
-                nums.pop(i)
-            else:
-                i += 1
+        nums[:] = sorted(set(nums))
         return len(nums)
