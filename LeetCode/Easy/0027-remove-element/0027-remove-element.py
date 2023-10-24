54414f38e08e3a800c8e1eae79d3ddfd6213a4b7
Hyemin Kim <hmk9667@gmail.com>
2023-10-24 14:41:06 +0900

    Time: 38 ms (75.43%), Space: 16.4 MB (14.87%) - LeetHub

--- LeetCode/Easy/0027-remove-element/0027-remove-element.py
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.py
@@ -1,17 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         # nums의 val을 제거하는데 남아있는 nums의 앞에서부터 k개까지는 val이 없어야됨
-        # 투포인터를 이용하여 val을 발견하면 뒤를 가르키는 포인터 위치와 val 위치를 swap함
-        last_p = len(nums) -1 
-        now_p = last_p
+        # pop을 이용한 풀이 => 시간 복잡도 O(n)
         k = 0
-        while now_p >=0:
-            if nums[now_p] == val:
-                nums[now_p], nums[last_p] = nums[last_p], nums[now_p]
+        j = 0
+        all_len = len(nums)
+        while j < len(nums):
+            print(j, nums[j])
+            if nums[j] == val:
+                nums.pop(j)
                 k += 1
-                last_p -= 1
-            now_p -= 1
-        
-        return len(nums) - k
-            
-        
+            else:
+                j += 1
+        return all_len-k
