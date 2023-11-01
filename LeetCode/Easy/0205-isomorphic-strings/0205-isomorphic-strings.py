19080c788da8f580efe3c24819006af5cefc8739
Hyemin Kim <hmk9667@gmail.com>
2023-11-01 15:07:05 +0900

    Time: 47 ms (53.61%), Space: 16.6 MB (56.90%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,13 +2,16 @@ from collections import Counter
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         # 1. 개수대로 맵핑 
-        if len(set(s))!= len(set(t)) : # 1차 필터링
-            return False
+#         if len(set(s))!= len(set(t)) : # 1차 필터링
+#             return False
         
         mapping = {}
         for idx, c in enumerate(s):
             if c not in mapping.keys():
-                mapping[c] = t[idx]
+                if t[idx] not in mapping.values():
+                    mapping[c] = t[idx]
+                else:
+                    return False
             else:
                 if mapping[c]!= t[idx]:
                     return False 
