c04df1655f6572161e1b3e8b573899d17539300d
Hyemin Kim <hmk9667@gmail.com>
2023-11-01 14:27:54 +0900

    Time: 47 ms (90.52%), Space: 16.5 MB (71.74%) - LeetHub

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
@@ -1,7 +1,7 @@
 from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # 1. 단순 딕셔너리를 이용한 풀이
+#        # 1. 단순 딕셔너리를 이용한 풀이 => 순회 2번
 #         alpha = {}
 #         for m in magazine:
 #             alpha[m] = alpha.get(m, 0) + 1
@@ -12,7 +12,7 @@ class Solution:
 #             alpha[r] -= 1
 #         return True
 
-#         # 2. Counter를 이용한 풀이
+#         # 2. Counter를 이용한 풀이 => 순회 2번 
 #         m_count = Counter(magazine)
 #         r_count = Counter(ransomNote)
 #         m_count.subtract(r_count) 
@@ -22,7 +22,7 @@ class Solution:
 #                 return False
 #             return True
         
-        # 3. 문자열을 이용한 풀이
+        # 3. 문자열을 이용한 풀이 => O(n) 순회 1번 
         for r in ransomNote:
             if r in magazine:
                 magazine = magazine.replace(r,"",1)
