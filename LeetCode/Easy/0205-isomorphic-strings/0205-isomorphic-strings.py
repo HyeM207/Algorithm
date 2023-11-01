eedaa2e508f26df35504bef276dc0c05519dc7fa
Hyemin Kim <hmk9667@gmail.com>
2023-11-01 15:15:20 +0900

    Time: 46 ms (59.61%), Space: 16.7 MB (30.84%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,18 +1,18 @@
 from collections import Counter
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        # 1차 풀이 (필터링 있이) => 40ms
-#         if len(set(s))!= len(set(t)) : # 1차 필터링
-#             return False
+        # 1차 풀이 (필터링 있이) => 38ms
+        if len(set(s))!= len(set(t)) : # 1차 필터링
+            return False
         
-#         mapping = {}
-#         for idx, c in enumerate(s):
-#             if c not in mapping.keys():
-#                     mapping[c] = t[idx]
-#             else:
-#                 if mapping[c]!= t[idx]:
-#                     return False 
-#         return True
+        mapping = {}
+        for idx, c in enumerate(s):
+            if c not in mapping.keys():
+                    mapping[c] = t[idx]
+            else:
+                if mapping[c]!= t[idx]:
+                    return False 
+        return True
     
         # 2차 풀이 (필터링 없이) => 47ms 
         # mapping = {}
@@ -28,5 +28,5 @@ class Solution:
         # return True
         
         # 방법 2 : zip과 set을 이용하여 가능한 순서쌍의 중복 없앰
-        return len(set(s))==len(set(t))==len(set(zip(s,t))) 
+        # return len(set(s))==len(set(t))==len(set(zip(s,t))) 
         
