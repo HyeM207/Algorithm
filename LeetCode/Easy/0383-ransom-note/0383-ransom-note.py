d6ade21f4605db8c0bbdf072b4fbccf3b727de46
Hyemin Kim <hmk9667@gmail.com>
2023-11-01 14:20:06 +0900

    Time: 63 ms (54.58%), Space: 16.6 MB (12.92%) - LeetHub

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,23 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         # 1. 단순 딕셔너리를 이용한 풀이
-        alpha = {}
-        for m in magazine:
-            alpha[m] = alpha.get(m, 0) + 1
+#         alpha = {}
+#         for m in magazine:
+#             alpha[m] = alpha.get(m, 0) + 1
 
-        for r in ransomNote:
-            if alpha.get(r, 0) <= 0:
+#         for r in ransomNote:
+#             if alpha.get(r, 0) <= 0:
+#                 return False
+#             alpha[r] -= 1
+#         return True
+
+        # 2. Counter를 이용한 풀이
+        m_count = Counter(magazine)
+        r_count = Counter(ransomNote)
+        m_count.subtract(r_count) 
+        remain = sorted(list(m_count.values()), reverse=True)
+        while remain:
+            if remain.pop() < 0:
                 return False
-            alpha[r] -= 1
-        return True
+            return True
