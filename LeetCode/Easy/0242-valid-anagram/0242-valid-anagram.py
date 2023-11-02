4424320b76fd2a4c9028853a29517862f7203d35
Hyemin Kim <hmk9667@gmail.com>
2023-11-02 14:00:55 +0900

    Time: 45 ms (90.53%), Space: 16.7 MB (90.75%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -2,7 +2,22 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         """
         생각한 방법들 
-        1. 딕셔너리로 mapping -> 순회 필요
-        2. 문자열 sort로 바로 비교
+        1. 문자열 sort로 바로 비교
+        2. 딕셔너리로 mapping -> 순회 필요
         """
-        return sorted(s) == sorted(t)
+        # 방법 1 : 문자열 sort로 바로 비교
+        # return sorted(s) == sorted(t)
+    
+        # 방법 2 : 딕셔너리로 mapping -> 순회 필요
+        mapping = {} # 키는 알파벳, value는 사용한 횟수
+        
+        if len(s) != len(t) or len(set(s)) != len(set(t)):
+            return False
+        for a, b in zip(s, t):
+            mapping[a] = mapping.get(a, 0) + 1
+            mapping[b] = mapping.get(b, 0) - 1
+        for v in mapping.values():
+            if v < 0:
+                return False
+        return True
+        
