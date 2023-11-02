548660c74574844adf06c1143339144cc09fb542
Hyemin Kim <hmk9667@gmail.com>
2023-11-02 14:02:56 +0900

    Time: 55 ms (52.81%), Space: 16.6 MB (90.75%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -5,19 +5,18 @@ class Solution:
         1. 문자열 sort로 바로 비교
         2. 딕셔너리로 mapping -> 순회 필요
         """
-        # 방법 1 : 문자열 sort로 바로 비교
-        return sorted(s) == sorted(t)
+        # 방법 1 : 문자열 sort로 바로 비교 
+        # return sorted(s) == sorted(t)
     
-        # 방법 2 : 딕셔너리로 mapping -> 순회 필요
-#         mapping = {} # 키는 알파벳, value는 사용한 횟수
+        # 방법 2 : 딕셔너리로 mapping -> 순회 필요 ==> 실행시간 더 빠름
+        mapping = {} # 키는 알파벳, value는 사용한 횟수
         
-#         if len(s) != len(t) or len(set(s)) != len(set(t)):
-#             return False
-#         for a, b in zip(s, t):
-#             mapping[a] = mapping.get(a, 0) + 1
-#             mapping[b] = mapping.get(b, 0) - 1
-#         for v in mapping.values():
-#             if v < 0:
-#                 return False
-#         return True
-        
+        if len(s) != len(t) or len(set(s)) != len(set(t)):
+            return False
+        for a, b in zip(s, t):
+            mapping[a] = mapping.get(a, 0) + 1
+            mapping[b] = mapping.get(b, 0) - 1
+        for v in mapping.values():
+            if v < 0:
+                return False
+        return True
