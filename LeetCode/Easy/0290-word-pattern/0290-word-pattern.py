c7a7b58144b5bd58684c3ae14c01b25c9540030c
Hyemin Kim <hmk9667@gmail.com>
2023-11-02 13:38:20 +0900

    Time: 40 ms (42.59%), Space: 16.2 MB (47.72%) - LeetHub

--- LeetCode/Easy/0290-word-pattern/0290-word-pattern.py
+++ LeetCode/Easy/0290-word-pattern/0290-word-pattern.py
@@ -1,19 +1,36 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         
-        #  패턴의 각 문자를 key로하는 딕셔너리로 관리 => n회 순회
+        #  풀이 1. 패턴의 각 문자를 key로하는 딕셔너리로 관리 => n회 순회
+#         mapping = {}
+#         words = s.split()
+        
+#         if len(pattern) != len(words):
+#             return False
+#         for idx, p in enumerate(pattern):
+#             if p not in mapping.keys(): # 새로운 key일 경우 # if p not in mapping:
+#                 if words[idx] not in mapping.values():
+#                     mapping[p] = words[idx]
+#                 else:
+#                     return False
+#             else:
+#                 if mapping[p] != words[idx]: # 기존의 key 값과 다를 경우 
+#                     return False
+#         return True
+
+        # 풀이 2. if 필터링 후 n회 순회, if-elif-else문으로 코드 간단화함
         mapping = {}
         words = s.split()
-        
-        if len(pattern) != len(words):
+        if len(set(pattern)) != len(set(words)):
             return False
-        for idx, p in enumerate(pattern):
-            if p not in mapping.keys(): # 새로운 key일 경우 
-                if words[idx] not in mapping.values():
-                    mapping[p] = words[idx]
-                else:
-                    return False
+        if len(words) != len(pattern):
+            return False
+        
+        for i in range(len(pattern)):
+            if pattern[i] not in mapping:
+                mapping[pattern[i]] = words[i]
+            elif mapping[pattern[i]] == words[i]:
+                continue # 중복 적재 막음
             else:
-                if mapping[p] != words[idx]: # 기존의 key 값과 다를 경우 
-                    return False
+                return False
         return True
