a3bf362616cab4eaa2236febb90d30d5ea94f032
Hyemin Kim <hmk9667@gmail.com>
2023-11-02 14:06:11 +0900

    Time: 41 ms (96.16%), Space: 16.7 MB (90.75%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -9,14 +9,18 @@ class Solution:
         # return sorted(s) == sorted(t)
     
         # 방법 2 : 딕셔너리로 mapping -> 순회 필요 ==> 실행시간 더 빠름
-        mapping = {} # 키는 알파벳, value는 사용한 횟수
+#         mapping = {} # 키는 알파벳, value는 사용한 횟수
         
-        if len(s) != len(t) or len(set(s)) != len(set(t)):
-            return False
-        for a, b in zip(s, t):
-            mapping[a] = mapping.get(a, 0) + 1
-            mapping[b] = mapping.get(b, 0) - 1
-        for v in mapping.values():
-            if v < 0:
-                return False
-        return True
+#         if len(s) != len(t) or len(set(s)) != len(set(t)):
+#             return False
+#         for a, b in zip(s, t):
+#             mapping[a] = mapping.get(a, 0) + 1
+#             mapping[b] = mapping.get(b, 0) - 1
+#         for v in mapping.values():
+#             if v < 0:
+#                 return False
+#         return True
+
+        # 방법 3: Counter 이용 (방법 2를 Counter로 모두 대체함)
+        from collections import Counter
+        return Counter(s) == Counter(t)
