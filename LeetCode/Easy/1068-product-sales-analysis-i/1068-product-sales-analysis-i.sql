b877c69b74bcad442b99adcad117304e2d6b96b8
Hyemin Kim <hmk9667@gmail.com>
2023-11-03 11:17:45 +0900

    Time: 2068 ms (43.31%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Easy/1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ LeetCode/Easy/1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
 SELECT p.product_name, s.year, s.price
-FROM Product p
-    INNER JOIN Sales s
-    ON p.product_id = s.product_id;
+FROM Sales s
+JOIN Product p ON s.product_id = p.product_id;
