722383cc3c823d7273ac49b2d09738d4b4e0b378
Hyemin Kim <hmk9667@gmail.com>
2023-11-03 12:25:33 +0900

    Time: 645 ms (8.89%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,10 +1,19 @@
 # Write your MySQL query statement below
-SELECT t.machine_id, ROUND(AVG(processing_time), 3) as processing_time
-FROM (
-        SELECT p.machine_id, p.process_id, SUM(pc.timestamp - p.timestamp) as processing_time
-        FROM Activity p 
-            JOIN Activity pc
-            ON p.process_id = pc.process_id and p.machine_id = pc.machine_id and pc.activity_type = 'end' 
-        GROUP BY p.machine_id, p.process_id
-    )t
-GROUP BY t.machine_id;
+# 풀이 1: 서브쿼리와 self join 활용
+# SELECT t.machine_id, ROUND(AVG(processing_time), 3) as processing_time
+# FROM (
+#         SELECT p.machine_id, p.process_id, SUM(pc.timestamp - p.timestamp) as processing_time
+#         FROM Activity p 
+#             JOIN Activity pc
+#             ON p.process_id = pc.process_id and p.machine_id = pc.machine_id and pc.activity_type = 'end' 
+#         GROUP BY p.machine_id, p.process_id
+#     ) t
+# GROUP BY t.machine_id;
+
+# 풀이 2: self join 활용
+SELECT p.machine_id, ROUND(AVG(pc.timestamp - p.timestamp), 3) as processing_time
+FROM Activity p 
+    JOIN Activity pc
+    ON p.process_id = pc.process_id and p.machine_id = pc.machine_id and 
+    p.activity_type = 'start' and pc.activity_type = 'end'
+GROUP BY p.machine_id;
