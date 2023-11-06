323f066d0a38ce5ccce888575b6f5b98529a905d
Hyemin Kim <hmk9667@gmail.com>
2023-11-06 11:47:04 +0900

    Time: 31 ms (92.48%), Space: 16.2 MB (57.34%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,28 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # 풀이 1: 1회 순회하며 stack과 if문 분기 이용하여 풀이함 
+#         stack = []
+#         match = { '}' : '{', ')' : '(', ']': '['}
+        
+#         for val in s:
+#             if val in ('(', '[', '{'):
+#                 stack.append(val)
+#             elif val in (')', ']', '}'):
+#                 if not stack or stack.pop() != match[val]:
+#                     return False
+#             else:
+#                 return False
+        
+#         return False if stack else True 
+    
+        # 풀이 2: 풀이 1에서 불필요한 if문 제거함 
         stack = []
         match = { '}' : '{', ')' : '(', ']': '['}
         
         for val in s:
             if val in ('(', '[', '{'):
                 stack.append(val)
-            elif val in (')', ']', '}'):
-                if not stack or stack.pop() != match[val]:
+            elif not stack or stack.pop() != match[val]:
                     return False
-            else:
-                return False
         
         return False if stack else True 
