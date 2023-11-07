7bc58abbb9e710fa1efa705cb93a843c237ed74e
Hyemin Kim <hmk9667@gmail.com>
2023-11-07 17:08:29 +0900

    Time: 55 ms (72.09%), Space: 20 MB (98.61%) - LeetHub

--- LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.py
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -3,23 +3,33 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-"""
-중복된 값이 노드 값으로 들어갈 수 있음
-사이클인지 확인 방법은?
-    - 끝까지 가본다 (순회 횟수 넘어가면 True)
-    - node의 주소값  X 
-"""
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return False
-        p = head
-        nodes = []
-        while p:
-            if p in nodes:
+        # 풀이1. node를 리스트 저장하여 확인  
+#         if not head:
+#             return False
+#         p = head
+#         nodes = []
+#         while p:
+#             print(nodes)
+#             if p in nodes:
+#                 return True
+#             nodes.append(p)
+#             if not p.next:
+#                 return False
+#             p = p.next
+#         return True
+    
+        # 풀이2. Floyd's tortoise and hare - 투포인터 이용
+        """
+        fast는 2개씩 앞섬
+        slow는 1개씩 앞섬
+        """
+        fast = slow = head
+        while fast and fast.next :
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
                 return True
-            nodes.append(p)
-            if not p.next:
-                return False
-            p = p.next
-        return True
+        return False
