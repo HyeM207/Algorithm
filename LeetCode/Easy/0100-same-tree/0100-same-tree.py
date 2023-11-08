57184e322c43600e234264c2f23d5a359d46cefc
Hyemin Kim <hmk9667@gmail.com>
2023-11-08 13:13:21 +0900

    Time: 37 ms (67.14%), Space: 16.2 MB (59.00%) - LeetHub

--- LeetCode/Easy/0100-same-tree/0100-same-tree.py
+++ LeetCode/Easy/0100-same-tree/0100-same-tree.py
@@ -5,22 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def dfs(head, nodes):
-            if not head:
-                return nodes
-            left = []
-            right = []
-            if head.left:
-                left = dfs(head.left, nodes+[head.left.val])
-            else:
-                left = dfs(head.left, nodes+['null'])
-            if head.right :
-                right = dfs(head.right, nodes+[head.right.val]) 
-            else:
-                right = dfs(head.right, nodes+['null'])
-            return left+right +[head.val]
+    # 풀이1: dfs 함수로 각 트리를 순회하여 노드들을 리스트에 담고, 둘의 리스트를 비교함
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+#         def dfs(head, nodes):
+#             if not head:
+#                 return nodes
+#             left = []
+#             right = []
+#             if head.left:
+#                 left = dfs(head.left, nodes+[head.left.val])
+#             else:
+#                 left = dfs(head.left, nodes+['null'])
+#             if head.right :
+#                 right = dfs(head.right, nodes+[head.right.val]) 
+#             else:
+#                 right = dfs(head.right, nodes+['null'])
+#             return left+right +[head.val]
             
-        result1 = dfs(p, [])
-        result2 = dfs(q, [])
-        return result1==result2
+#         result1 = dfs(p, [])
+#         result2 = dfs(q, [])
+#         return result1==result2
+
+    # 풀이2: 재귀함수 (더 효율적임)
+    # 동시에 p와 q의 left와 right을 비교해나감
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None and q == None :
+            return True
+
+        if p == None or q == None or p.val != q.val :
+            return False
+
+        return (self.isSameTree(p.left , q.left) and self.isSameTree(p.right , q.right))    
