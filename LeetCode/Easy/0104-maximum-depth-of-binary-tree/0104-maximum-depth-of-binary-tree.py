dad82c2f66c4cf06e460c64ff2494caaf45ce731
Hyemin Kim <hmk9667@gmail.com>
2023-11-08 11:45:34 +0900

    Time: 50 ms (27.65%), Space: 18.7 MB (15.27%) - LeetHub

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,17 +6,23 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        def dfs(depth,  node):
-            global max_depth
-            max_depth = max(max_depth, depth)
-            if node.left :
-                dfs(depth+1, node.left)
-            if node.right :
-                dfs(depth+1, node.right)        
+        # 풀이1. dfs 와 전역 변수 이용 
+#         def dfs(depth,  node):
+#             global max_depth
+#             max_depth = max(max_depth, depth)
+#             if node.left :
+#                 dfs(depth+1, node.left)
+#             if node.right :
+#                 dfs(depth+1, node.right)        
 
+#         if not root:
+#             return 0
+#         global max_depth
+#         max_depth = 1
+#         dfs(1, root)
+#         return max_depth
+        
+        # 풀이2. 재귀 함수 이용
         if not root:
             return 0
-        global max_depth
-        max_depth = 1
-        dfs(1, root)
-        return max_depth
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
