01295b21418f472a68ccc2239fc7ba842b31f5fd
Hyemin Kim <hmk9667@gmail.com>
2023-11-08 12:35:48 +0900

    Time: 55 ms (5.76%), Space: 18.8 MB (5.16%) - LeetHub

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -24,6 +24,7 @@ class Solution:
 
     # 풀이2. 본 함수를 재귀 함수 이용
     # dfs로 leaf 노드에 도달했다가 올라오면서 최종적으로 depth 계산이 됨 
+    # 잎(leaf) 노드에 도달하면 깊이 1로 시작하고, 이를 상위 노드로 전달하며 계산합니다.
     # def maxDepth1(self, root: Optional[TreeNode]) -> int:
     #     if not root:
     #         return 0
@@ -32,6 +33,7 @@ class Solution:
     
     # 풀이3. 풀이2 변형 (더 효율적임)
     # dfs로  leaf 노드에 도달했을 때 depth가 도출됨. 그리고 재귀 함수 종료  
+    # 잎(leaf) 노드에서 최대 깊이를 계산한 후, 상위 노드로 올라가며 가장 큰 깊이를 선택합니다.
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         def dfs(root, depth):
             if not root: 
