9548f5d8633e818e730a736c398e68e3f14edcd6
Hyemin Kim <hmk9667@gmail.com>
2023-11-08 12:30:35 +0900

    Time: 47 ms (47.63%), Space: 18.8 MB (15.27%) - LeetHub

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -5,8 +5,8 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # 풀이1. dfs 와 전역 변수 이용 
 #     def maxDepth(self, root: Optional[TreeNode]) -> int:
-#         # 풀이1. dfs 와 전역 변수 이용 
 #         def dfs(depth,  node):
 #             global max_depth
 #             max_depth = max(max_depth, depth)
@@ -21,8 +21,23 @@ class Solution:
 #         max_depth = 1
 #         dfs(1, root)
 #         return max_depth
-        def maxDepth(self, root: Optional[TreeNode]) -> int:
-            # 풀이2. 재귀 함수 이용
-            if not root:
-                return 0
-            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+    # 풀이2. 본 함수를 재귀 함수 이용
+    # dfs로 leaf 노드에 도달했다가 올라오면서 최종적으로 depth 계산이 됨 
+    # def maxDepth1(self, root: Optional[TreeNode]) -> int:
+    #     if not root:
+    #         return 0
+    #     return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+    
+    
+    # 풀이3. 풀이2 변형 (더 효율적임)
+    # dfs로  leaf 노드에 도달했을 때 depth가 도출됨. 그리고 재귀 함수 종료  
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, depth):
+            if not root: 
+                print("not root")
+                return depth
+            print(root.val, depth)
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+                       
+        return dfs(root, 0)
