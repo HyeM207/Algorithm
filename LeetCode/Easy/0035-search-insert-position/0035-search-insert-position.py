a458901ba56b9a47c33bfdb9a69d6a6c5172e759
Hyemin Kim <hmk9667@gmail.com>
2023-11-14 12:24:42 +0900

    Time: 59 ms (24.47%), Space: 17.3 MB (20.14%) - LeetHub

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.py
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.py
@@ -1,10 +1,11 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        """
+    """
         target이 있으면 인덱스를 없으면, 넣어야되는 인덱스 반환
         - 탐색 시간 제한있음
         - 반으로 나눠서 탐색하는 분할 탐색 
-        """
+    """
+    # 풀이1 : 만든 재귀함수 이용
+    def searchInsert_(self, nums: List[int], target: int) -> int:
         def search(left, right, target):
             # 1) 타켓이 없을 경우 삽입할 인덱스 리턴
             if left > right:
@@ -27,3 +28,17 @@ class Solution:
             return result
         
         return search(0, len(nums)-1, target)
+    
+    # 풀이 2: 간단하게 풀이
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid -1 
+            else:
+                return mid
+        return left
