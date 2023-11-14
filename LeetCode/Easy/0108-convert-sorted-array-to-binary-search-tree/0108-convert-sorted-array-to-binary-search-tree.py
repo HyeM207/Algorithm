c2949cd8e4166136917076209770f2feac48d317
Hyemin Kim <hmk9667@gmail.com>
2023-11-14 12:30:07 +0900

    Time: 58 ms (78.51%), Space: 17.9 MB (14.33%) - LeetHub

--- LeetCode/Easy/0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ LeetCode/Easy/0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -5,12 +5,13 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        """ 
-        # 분할정복
-        한 그룹의 중앙값을 BST 트리에 배치
-         -> 그 값을 제외한 양쪽 그룹에서 또 중앙값 찾고 배치 -> 반복
-        """
+    """ 
+    # 분할정복
+    한 그룹의 중앙값을 BST 트리에 배치
+     -> 그 값을 제외한 양쪽 그룹에서 또 중앙값 찾고 배치 -> 반복
+    """
+    # 풀이 1: 만든 재귀함수 이용
+    def sortedArrayToBST_(self, nums: List[int]) -> Optional[TreeNode]:
         def divide(left, right):
             if left > right: # 종료 조건 기억해두기
                 return None
@@ -21,3 +22,13 @@ class Solution:
             return node
         
         return divide(0, len(nums)-1)
+
+    # 풀이 2: 주어진 함수를 재귀함수로 이용
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
