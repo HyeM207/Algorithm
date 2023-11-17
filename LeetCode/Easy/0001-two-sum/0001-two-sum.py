4897d51e48a7695c03a0575a43c3fb7fe8677c7f
Hyemin Kim <hmk9667@gmail.com>
2023-11-17 13:37:41 +0900

    Time: 62 ms (75.91%), Space: 17.8 MB (5.93%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -7,7 +7,7 @@ class Solution:
                     return [i,j]
                 
     # 풀이 2. 딕셔너리화 -> 정렬 후 투포인터로 합 더하기 => O(n log n)
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum_(self, nums: List[int], target: int) -> List[int]:
         # 1) 딕셔너리화 및 정렬
         # 이때 중복된 값이 들어갈 수 있으므로 배열로 인덱스 위치 저장 
         index = {}
@@ -28,4 +28,14 @@ class Solution:
         
         # 같은 값을 더하는 경우 예외 처리해줌
         return index[nums[left]]+ index[nums[right]] if nums[left]!=nums[right] else index[nums[left]]
+    
+    
+    # 풀이 3. 1회 순회와 해시맵 이용 => O(N)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap: # 해시맵을 이용하여 탐색 시간 줄임
+                return [i, hashmap[complement]]
+            hashmap[num] = i
         
