ac666083d78afbf7c04dd0a3fc1e976249dc00aa
Hyemin Kim <hmk9667@gmail.com>
2023-11-20 19:08:03 +0900

    Time: 45 ms (38.64%), Space: 16.2 MB (39.52%) - LeetHub

--- LeetCode/Easy/0202-happy-number/0202-happy-number.py
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.py
@@ -1,26 +1,25 @@
 class Solution:
+    # 풀이 1. 단순 구현 (코드 가독성 높임)
+    """
+    단순 구현
+    - 0~9까지의 제곱을 모두 구해놓고 저장 
+    - 이미 등장한 숫자인지 딕셔너리로 탐색  
+    """  
     def isHappy(self, n: int) -> bool:
-        """
-        단순 구현
-        - 0~9까지의 제곱을 모두 구해놓고 저장 
-        - 이미 등장한 숫자인지 딕셔너리로 탐색  
-        """        
         # 제곱 미리 계산하여 저장
         square = {}
         for i in range(0, 10):
             square[str(i)] = i*i
         
-        existed = {n:True} # 딕셔너리로 탐색시간 줄임
         # n 검증
-        while True:
-            hap = 0
-            for i in str(n):
-                hap += square[i]
-            if hap == 1:
-                return True
+        existed = {} 
+        hap = 0
+        while hap != 1:
+            hap = sum(int(square[i]) for i in str(n))
             n = hap
             if existed.get(n, 'False') is True: # 이미 등장했던 숫자인지 검토
                 return False
             existed[n] = True
+            
         return True if n == 1 else False
         
