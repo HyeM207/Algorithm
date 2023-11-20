0ce4f7b7e99ef5fc154e8f772ae3037d8c9b0012
Hyemin Kim <hmk9667@gmail.com>
2023-11-20 19:29:24 +0900

    Time: 40 ms (70.44%), Space: 16.3 MB (10.05%) - LeetHub

--- LeetCode/Easy/0202-happy-number/0202-happy-number.py
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.py
@@ -1,11 +1,11 @@
 class Solution:
-    # 풀이 1. 단순 구현 (코드 가독성 높임)
+    # 풀이 1. 딕셔너리를 이용한 단순 구현 
     """
     단순 구현
     - 0~9까지의 제곱을 모두 구해놓고 저장 
     - 이미 등장한 숫자인지 딕셔너리로 탐색  
     """  
-    def isHappy(self, n: int) -> bool:
+    def isHappy_(self, n: int) -> bool:
         # 제곱 미리 계산하여 저장
         square = {}
         for i in range(0, 10):
@@ -22,4 +22,11 @@ class Solution:
             existed[n] = True
             
         return True if n == 1 else False
-        
+    
+    # 풀이 2: set을 활용한 단순 구현
+    def isHappy(self, n: int) -> bool:
+        seen = set()  # 이미 방문한 숫자를 기록하기 위한 집합(set)
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit) ** 2 for digit in str(n))
+        return n == 1
