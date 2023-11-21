31f64d3d12be17f3164932803c68af2abd65f6c6
Hyemin Kim <hmk9667@gmail.com>
2023-11-21 11:53:59 +0900

    Time: 32 ms (89.43%), Space: 16.4 MB (23.29%) - LeetHub

--- LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.py
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,4 +1,16 @@
 class Solution:
-    # 풀이1. 내장 함수 이용한 한 줄 코드
+    # 풀이1. 내장 함수 이용한 한 줄 코드 <- 더 나음
     def lengthOfLastWord(self, s: str) -> int:
         return len(s.split()[-1])
+    
+    # 풀이2: 인덱스 접근 
+    def lengthOfLastWord(self, s: str) -> int:
+        result = 0
+        is_started = False
+        for i in range(len(s)-1 ,-1, -1):
+            if s[i] == ' ' and is_started:
+                break
+            if s[i] != ' ':
+                result += 1
+                is_started = True
+        return result
