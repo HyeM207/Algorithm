2e97c2c1cd66dd3b0e10f770a6a8c1c66fa3b9b2
Hyemin Kim <hmk9667@gmail.com>
2023-11-21 11:28:58 +0900

    Time: 50 ms (66.39%), Space: 16.4 MB (18.34%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,32 +1,24 @@
 class Solution:
+    # 풀이 1: 딕셔너리 이용하여 1회 순회
     def romanToInt(self, s: str) -> int:
-        roman = {
-            'I' : 1,
-            'V' : 5,
-            'X' : 10,
-            'L' : 50,
-            'C' : 100,
-            'D' : 500,
-            'M' : 1000
-        }
-        roman_four = {
-            "IV" : 4,
-            "IX" : 9,
-            "XL" : 40,
-            "XC" : 90,
-            "CD" : 400,
-            "CM" : 900
-        }
-        answer = 0
+        """
+        값은 딕셔너리화 하고,for문으로 1회만 돌자
+            - 1회 돌때 이전 값을 더함
+            - 이유: 이전값이랑 현재값 더하여 예외케이스일 수 있음
+        """
+        symbols = { 'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000 ,
+                  'IV' : 4, 'IX' : 9, 'XL' : 40 , 'XC' : 90, 'CD' : 400, 'CM' : 900 }
+        
+        result = 0
         prev = s[0]
-        answer += roman.get(prev, 0)
         for c in s[1:]:
-            print(prev+c)
-            if prev+c in roman_four.keys():
-                answer -= roman.get(prev, 0)
-                answer += roman_four.get(prev+c, 0)
-                
+            if prev and prev+c in symbols:
+                result += symbols.get(prev+c, 0)
+                prev = None
             else:
-                answer += roman.get(c, 0)
-            prev=c
-        return answer
+                result += symbols.get(prev, 0)
+                prev = c   
+        result += symbols.get(prev, 0)
+        
+        return result
+    
