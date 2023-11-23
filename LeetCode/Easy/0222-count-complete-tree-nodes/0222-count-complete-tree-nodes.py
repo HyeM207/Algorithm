187b2b5b2912e632703270f1f5e3a6d2784d7a1e
Hyemin Kim <hmk9667@gmail.com>
2023-11-23 12:34:02 +0900

    Time: 66 ms (89.40%), Space: 23.8 MB (8.32%) - LeetHub

--- LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,8 +5,8 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    #  풀이 1: bfs로 deque를 활용하여 풀기
-    def countNodes(self, root: Optional[TreeNode]) -> int:
+    #  풀이 1: bfs로 deque를 활용하여 풀기 => O(N)
+    def countNodes_(self, root: Optional[TreeNode]) -> int:
         from collections import deque
         
         if not root:
@@ -24,3 +24,40 @@ class Solution:
                 queue.append(current_node.right)
             
         return answer
+    
+    # 풀이 2 :  완전이진트리 특성 이용  => O(log^2 N)
+    # 참고: https://leetcode.com/problems/count-complete-tree-nodes/discuss/2815567/python3-general-Binary-Search-using-template
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        """
+        1. 깊이 구함 (맨 왼쪽 기준) - root부터 leaf노드 위치한 그 위의 높이까지 노드 개수 계산 가능
+        2. 마지막 leaf 노드 개수 확인 
+        """
+        if not root: return 0
+        
+        def findDepth(node):
+            if not node.left:
+                return 0
+            return findDepth(node.left)+1
+
+        def check(node,mid,curDepth,left,right):
+            # 찾고자 하는 depth의 노드라면 return True
+            if curDepth == depth: return True if node else False 
+            # 그게 아니면 mid값 조정해서 탐색 범위 변경함
+            if mid <= (left+right)//2:
+                return check(node.left,mid,curDepth+1,left,(left+right)//2)
+            else:
+                return check(node.right,mid,curDepth+1,(left+right)//2,right)
+        
+        depth = findDepth(root)
+        lastLevelLength = 2**depth
+        
+        # s와 e는 마지막 depth의 노드 시작&끝 인덱스 
+        s, e = 1, lastLevelLength+1
+        while s+1 < e:
+            mid = (s+e)//2
+            if check(root,mid,0,1,lastLevelLength):
+                s = mid
+            else:
+                e = mid
+
+        return lastLevelLength - 1 + s
