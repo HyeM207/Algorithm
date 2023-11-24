015a4c67a84588839a8d3b37a5728de2188aec20
Hyemin Kim <hmk9667@gmail.com>
2023-11-24 11:30:47 +0900

    Time: 34 ms (92.45%), Space: 16.4 MB (9.21%) - LeetHub

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.py
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,13 +1,13 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    # 풀이 1: 이중for문 -> O(NM) ;M은 가장 짧은 문자열의 길이
+    def longestCommonPrefix_(self, strs: List[str]) -> str:
         """
         인덱스 하나씩 늘려가며 모든 단어 조회 -> 인덱스는 가장 짧은 단어 길이 만큼만
-            sort&len을 쓰는게 빠를까 
-        최대 약 40000번 조회 (ok)
+        최대 약 40000번 조회
         """
-        
         answer = ""
         sorted_strs= sorted(strs, key= lambda x: len(x))
+        
         for i in range(len(sorted_strs[0])):
             letter = sorted_strs[0][i]
             for c in sorted_strs[1:]:
@@ -15,3 +15,21 @@ class Solution:
                     return answer
             answer += letter
         return answer
+    
+    # 풀이 2 : for문 1회
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        min_len = min(len(s) for s in strs)
+        answer = ""
+
+        for i in range(min_len):
+            if all(s[i] == strs[0][i] for s in strs): # all 사용하여 코드 간단히 함
+                answer += strs[0][i]
+            else:
+                break
+                
+        return answer
+        
+        
