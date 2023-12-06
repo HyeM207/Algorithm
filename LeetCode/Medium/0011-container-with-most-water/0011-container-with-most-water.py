1358473753cf3ac3fe769b56cef7f516069c0cab
Hyemin Kim <hmk9667@gmail.com>
2023-12-06 23:59:37 +0900

    Time: 605 ms (44.81%), Space: 29.3 MB (62.57%) - LeetHub

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.py
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,5 +1,5 @@
 class Solution:
-    # 풀이 1: 투포인터 이용
+    # 풀이 1: 투포인터 이용=> 시간 복잡도 O(n), 공간복잡도 O(1)
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = len(height) - 1
