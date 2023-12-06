23ba095667af78ccc633b0d5d68ab296e26b4d3e
Hyemin Kim <hmk9667@gmail.com>
2023-12-07 00:02:17 +0900

    Time: 556 ms (94.64%), Space: 29.4 MB (5.42%) - LeetHub

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.py
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,6 +1,6 @@
 class Solution:
     # 풀이 1: 투포인터 이용=> 시간 복잡도 O(n), 공간복잡도 O(1)
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea_(self, height: List[int]) -> int:
         left = 0
         right = len(height) - 1
         answer = 0
@@ -10,6 +10,21 @@ class Solution:
             if height[left] < height[right]:
                 left += 1
             else:
+                right -= 1
+        
+        return answer
+    
+    #  풀이 2: 풀이1에서 넓이 계산시 계산 단순화
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        answer = 0
+        while left < right:
+            if height[left] < height[right]:
+                answer = max(answer, height[left] * (right-left))
+                left += 1
+            else:
+                answer = max(answer, height[right] * (right-left))
                 right -= 1
         
         return answer
