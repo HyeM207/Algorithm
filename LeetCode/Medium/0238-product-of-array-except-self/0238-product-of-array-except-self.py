d95f39458cd614e6cd67d1646f4b3f81b115b7ad
Hyemin Kim <hmk9667@gmail.com>
2023-12-08 13:13:03 +0900

    Time: 223 ms (6.78%), Space: 24.2 MB (41.68%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 풀이1 : 왼쪽,오른쪽 곱 이용함. 하나의 for문을 이용한 풀이
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         """
         나누기 없이 진행이라..
@@ -14,12 +15,11 @@ class Solution:
         right_multi = 1
         for i in range(len_n):
             # 왼쪽 곱셈
-            if i != 0:
-                left_multi *= nums[i-1]
             answer[i] *= left_multi
+            left_multi *= nums[i]
+
             # 오른쪽 곱셈
-            if i != 0:
-                right_multi *= nums[len_n-i]
-            answer[len_n-i-1] *= right_multi
-        
+            answer[len_n - i - 1] *= right_multi
+            right_multi *= nums[len_n - i - 1]
+
         return answer
