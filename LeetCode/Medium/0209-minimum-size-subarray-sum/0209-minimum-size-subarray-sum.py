d6e9140ac3f46867ef8b489cdf53771a414cb84d
Hyemin Kim <hmk9667@gmail.com>
2023-12-12 22:05:08 +0900

    Time: 209 ms (93.92%), Space: 29.6 MB (11.48%) - LeetHub

--- LeetCode/Medium/0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ LeetCode/Medium/0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def minSubArrayLen_(self, target: int, nums: List[int]) -> int:
         """
-        풀이 1 : 투포인터
+        풀이 1 : 투포인터 (234ms)
             - 오른쪽 포인터는 합이 target보다 작을때 이동
             - 왼쪽 포인터는 합이 target보다 커졌을 때 이동
         """
@@ -27,9 +27,9 @@ class Solution:
         return answer
     
     
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+    def minSubArrayLen_(self, target: int, nums: List[int]) -> int:
         """
-        풀이 2 : 투포인터 (풀이 1 개선)
+        풀이 2 : 투포인터 (풀이 1 개선) (229ms)
         """
         left, right = 0, 0
         hap = nums[0]
@@ -46,4 +46,22 @@ class Solution:
                     break
                 hap += nums[right]
 
+        return 0 if answer == float('inf') else answer
+    
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+        풀이 3 : 투포인터 (다른 방식의 투포인터)
+            - 오른쪽 포인터 고정 후 왼쪽 포인터 이동
+        """
+        left = 0 
+        hap = 0  # 합은 0에서 시작
+        answer = float('inf')
+
+        for right in range(len(nums)):
+            hap += nums[right]
+            while hap >= target: # hap이 클 때 왼쪽 포인터를 움직여 배열을 줄여감 
+                answer = min(answer, right-left+1)
+                hap -= nums[left]
+                left += 1
+                
         return 0 if answer == float('inf') else answer
