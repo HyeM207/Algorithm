5044bc68106326a7c48858db9b5074ee14177f1d
Hyemin Kim <hmk9667@gmail.com>
2023-12-17 22:27:23 +0900

    Time: 1975 ms (28.61%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Easy/1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ LeetCode/Easy/1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,9 +1,15 @@
-# Write your MySQL query statement below
-SELECT contest_id, ROUND(COUNT(user_id )/u.total_users *100, 2) as percentage 
+# 풀이 1 : INNER JOIN 이용 (1611ms)
+# SELECT contest_id, ROUND(COUNT(user_id )/u.total_users *100, 2) as percentage 
+# FROM Register 
+# INNER JOIN (
+#     SELECT COUNT(user_id) as total_users
+#     FROM Users
+# ) u
+# GROUP BY contest_id 
+# ORDER BY percentage DESC, contest_id ASC;
+
+# 풀이 2 : 서브쿼리 이용 
+SELECT contest_id, ROUND(COUNT(user_id) * 100 / (SELECT COUNT(user_id) FROM Users), 2) AS percentage
 FROM Register 
-INNER JOIN (
-    SELECT COUNT(user_id) as total_users
-    FROM Users
-) u
-GROUP BY contest_id 
-ORDER BY percentage DESC, contest_id ASC;
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
